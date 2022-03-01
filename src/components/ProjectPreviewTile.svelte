0abf01ccde304f0a32a1d7bfdcd61504ce67542f
Daniel Sharkov <{ID}+{username}@users.noreply.github.com>
2022-03-01 13:50:27 +0100

    fix: wrong icon

--- src/components/ProjectPreviewTile.svelte
+++ src/components/ProjectPreviewTile.svelte
@@ -137,8 +137,10 @@
 									</g>
 								</svg>
 							{:else if $lazyloader.status === LazyLoadStatus.ERR}
-								<svg class='icon icon-error icon-large fill' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' fill='none'>
-									<path d='M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z'/>
+								<svg class='icon icon-error icon-large' viewBox='0 0 64 64' fill='none' aria-hidden='true' focusable='false' role='presentation' xmlns='http://www.w3.org/2000/svg'>
+									<circle class='stroke' cx='32' cy='32' r='27' stroke-width='2'/>
+									<circle class='fill' cx='32' cy='46' r='2'/>
+									<path class='stroke' d='M32 16V38' stroke-width='3' stroke-linecap='round'/>
 								</svg>
 							{/if}
 						</div>
@@ -219,8 +221,10 @@
 									</g>
 								</svg>
 							{:else if $lazyloader.status === LazyLoadStatus.ERR}
-								<svg class='icon icon-error icon-large fill' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' fill='none'>
-									<path d='M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z'/>
+								<svg class='icon icon-error icon-large' viewBox='0 0 64 64' fill='none' aria-hidden='true' focusable='false' role='presentation' xmlns='http://www.w3.org/2000/svg'>
+									<circle class='stroke' cx='32' cy='32' r='27' stroke-width='2'/>
+									<circle class='fill' cx='32' cy='46' r='2'/>
+									<path class='stroke' d='M32 16V38' stroke-width='3' stroke-linecap='round'/>
 								</svg>
 							{/if}
 						</div>
