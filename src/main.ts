1c81d7d316e3f49cb1f9fc377cc55ade9c867a3a
Daniel Sharkov <26609752+DanielSharkov@users.noreply.github.com>
2022-04-22 01:23:03 +0200

    feat: predefined meta tags The predefined tags are required for search engines and site previews, because JavaScript is either not loaded at all or delayed and therefor the metadata is missing.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
 import App from './App.svelte'
 const app = new App({target: document.body})
 export default app
+
+// Remove predefined meta tags, because libraray "Svelte Meta Tags" doesn't
+// override them. The predefined tags are required for search engines and
+// site previews, because JS is either not loaded at all or delayed and
+// therefor the metadata is missing.
+function _removePredefinedMetaTags() {
+	document.querySelectorAll(
+		`meta[name='robots'], meta[name='googlebot'], meta[name='description'],`+
+		`meta[name^='twitter:'], meta[property^='og:']`
+	).forEach((metaTag)=> {
+		document.head.removeChild(metaTag)
+	})
+}
+_removePredefinedMetaTags()
